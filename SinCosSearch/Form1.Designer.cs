67821508db5bdf9b03f5a3d4d1fd8069a096340b
Eric Talviste <Eric.Talviste@tptlive.ee>
2018-06-07 15:15:10 +0300

    basics

--- SinCosSearch/Form1.Designer.cs
+++ SinCosSearch/Form1.Designer.cs
@@ -28,13 +28,209 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.TABSave = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.TATBxCOS = new System.Windows.Forms.TextBox();
+            this.SINCOSTANCTG = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.firstPlus = new System.Windows.Forms.Label();
+            this.secondPlus = new System.Windows.Forms.Label();
+            this.TABTeljed = new System.Windows.Forms.Button();
+            this.TABJoonista = new System.Windows.Forms.Button();
+            this.ColorL = new System.Windows.Forms.Label();
+            this.TABClear = new System.Windows.Forms.Button();
+            this.TABColor = new System.Windows.Forms.Button();
+            this.TAPBxCanvas = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TAPBxCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TABSave
+            // 
+            this.TABSave.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TABSave.Location = new System.Drawing.Point(12, 273);
+            this.TABSave.Name = "TABSave";
+            this.TABSave.Size = new System.Drawing.Size(135, 38);
+            this.TABSave.TabIndex = 23;
+            this.TABSave.Text = "&Salvesta";
+            this.TABSave.UseVisualStyleBackColor = true;
+            this.TABSave.Click += new System.EventHandler(this.TABSave_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TATBxCOS);
+            this.groupBox1.Controls.Add(this.SINCOSTANCTG);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.firstPlus);
+            this.groupBox1.Controls.Add(this.secondPlus);
+            this.groupBox1.Location = new System.Drawing.Point(12, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(135, 209);
+            this.groupBox1.TabIndex = 26;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Andmed";
+            // 
+            // TATBxCOS
+            // 
+            this.TATBxCOS.Location = new System.Drawing.Point(55, 147);
+            this.TATBxCOS.Name = "TATBxCOS";
+            this.TATBxCOS.Size = new System.Drawing.Size(42, 29);
+            this.TATBxCOS.TabIndex = 12;
+            this.TATBxCOS.Text = "0";
+            this.TATBxCOS.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // SINCOSTANCTG
+            // 
+            this.SINCOSTANCTG.Location = new System.Drawing.Point(55, 63);
+            this.SINCOSTANCTG.Name = "SINCOSTANCTG";
+            this.SINCOSTANCTG.Size = new System.Drawing.Size(42, 29);
+            this.SINCOSTANCTG.TabIndex = 6;
+            this.SINCOSTANCTG.Text = "0";
+            this.SINCOSTANCTG.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 22);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "SIN:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 22);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "COS:";
+            // 
+            // firstPlus
+            // 
+            this.firstPlus.AutoSize = true;
+            this.firstPlus.Location = new System.Drawing.Point(21, 66);
+            this.firstPlus.Name = "firstPlus";
+            this.firstPlus.Size = new System.Drawing.Size(20, 22);
+            this.firstPlus.TabIndex = 15;
+            this.firstPlus.Text = "+";
+            this.firstPlus.Click += new System.EventHandler(this.TALPlusMinus_Click);
+            // 
+            // secondPlus
+            // 
+            this.secondPlus.AutoSize = true;
+            this.secondPlus.Location = new System.Drawing.Point(21, 150);
+            this.secondPlus.Name = "secondPlus";
+            this.secondPlus.Size = new System.Drawing.Size(20, 22);
+            this.secondPlus.TabIndex = 16;
+            this.secondPlus.Text = "+";
+            this.secondPlus.Click += new System.EventHandler(this.TALPlusMinus_Click);
+            // 
+            // TABTeljed
+            // 
+            this.TABTeljed.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TABTeljed.Location = new System.Drawing.Point(12, 352);
+            this.TABTeljed.Name = "TABTeljed";
+            this.TABTeljed.Size = new System.Drawing.Size(135, 63);
+            this.TABTeljed.TabIndex = 25;
+            this.TABTeljed.Text = "Lisa Teljed";
+            this.TABTeljed.UseVisualStyleBackColor = true;
+            this.TABTeljed.Click += new System.EventHandler(this.TABTeljed_Click);
+            // 
+            // TABJoonista
+            // 
+            this.TABJoonista.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TABJoonista.Location = new System.Drawing.Point(12, 229);
+            this.TABJoonista.Name = "TABJoonista";
+            this.TABJoonista.Size = new System.Drawing.Size(135, 38);
+            this.TABJoonista.TabIndex = 24;
+            this.TABJoonista.Text = "Joonista";
+            this.TABJoonista.UseVisualStyleBackColor = true;
+            this.TABJoonista.Click += new System.EventHandler(this.TABJoonista_Click);
+            // 
+            // ColorL
+            // 
+            this.ColorL.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ColorL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ColorL.Location = new System.Drawing.Point(116, 424);
+            this.ColorL.Name = "ColorL";
+            this.ColorL.Size = new System.Drawing.Size(31, 69);
+            this.ColorL.TabIndex = 22;
+            // 
+            // TABClear
+            // 
+            this.TABClear.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TABClear.Location = new System.Drawing.Point(12, 512);
+            this.TABClear.Name = "TABClear";
+            this.TABClear.Size = new System.Drawing.Size(713, 47);
+            this.TABClear.TabIndex = 20;
+            this.TABClear.Text = "Puhasta";
+            this.TABClear.UseVisualStyleBackColor = true;
+            this.TABClear.Click += new System.EventHandler(this.TABClear_Click);
+            // 
+            // TABColor
+            // 
+            this.TABColor.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TABColor.Location = new System.Drawing.Point(12, 421);
+            this.TABColor.Name = "TABColor";
+            this.TABColor.Size = new System.Drawing.Size(97, 69);
+            this.TABColor.TabIndex = 21;
+            this.TABColor.Text = "Vali Värv";
+            this.TABColor.UseVisualStyleBackColor = true;
+            this.TABColor.Click += new System.EventHandler(this.TABColor_Click);
+            // 
+            // TAPBxCanvas
+            // 
+            this.TAPBxCanvas.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.TAPBxCanvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TAPBxCanvas.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.TAPBxCanvas.Location = new System.Drawing.Point(153, 12);
+            this.TAPBxCanvas.Name = "TAPBxCanvas";
+            this.TAPBxCanvas.Size = new System.Drawing.Size(572, 494);
+            this.TAPBxCanvas.TabIndex = 19;
+            this.TAPBxCanvas.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(742, 572);
+            this.Controls.Add(this.TABSave);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.TABTeljed);
+            this.Controls.Add(this.TABJoonista);
+            this.Controls.Add(this.ColorL);
+            this.Controls.Add(this.TABClear);
+            this.Controls.Add(this.TABColor);
+            this.Controls.Add(this.TAPBxCanvas);
+            this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Sin Cos Research";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TAPBxCanvas)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button TABSave;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox TATBxCOS;
+        private System.Windows.Forms.TextBox SINCOSTANCTG;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label firstPlus;
+        private System.Windows.Forms.Label secondPlus;
+        private System.Windows.Forms.Button TABTeljed;
+        private System.Windows.Forms.Button TABJoonista;
+        private System.Windows.Forms.Label ColorL;
+        private System.Windows.Forms.Button TABClear;
+        private System.Windows.Forms.Button TABColor;
+        private System.Windows.Forms.PictureBox TAPBxCanvas;
     }
 }
 
--- SinCosSearch/Form1.cs
+++ SinCosSearch/Form1.cs
@@ -12,9 +12,230 @@ namespace SinCosSearch
 {
     public partial class Form1 : Form
     {
+        private Pen CustomPen;
+
+        ColorDialog ColorD = new ColorDialog();
+
+        Pen GrayPen = new Pen(Color.Gray, 1);
+        Pen BlackPen = new Pen(Color.Black, 2);
+        Pen RedPen = new Pen(Color.Red, 2);
+
+        Graphics g;
+        Bitmap Canvas;
+
+        float xalg, xlopp, dx, yalg, ylopp, dy, xpoint, ypoint;
+
+        int Yn = 30;
+        int Xn = 40;
+
         public Form1()
         {
             InitializeComponent();
+
+            Canvas = new Bitmap(TAPBxCanvas.Size.Width, TAPBxCanvas.Size.Height);
+            g = Graphics.FromImage(Canvas); //Graphics.FromHwnd(TAPBxCanvas.Handle);
+
+            TAPBxCanvas.Image = Canvas;
+
+            xalg = (float)(-2 * Math.PI);
+            xlopp = (float)(2 * Math.PI);
+
+            dx = (float)(Math.PI / 2);
+            dy = 1;
+
+            yalg = -3;
+            ylopp = 3;
+
+            CustomPen = new Pen(Color.Red, 2);
+
+            xpoint = TAPBxCanvas.Width / (xlopp - xalg);
+            ypoint = TAPBxCanvas.Height / (ylopp - yalg);
+        }
+
+        private void TABSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SaveFD = new SaveFileDialog();
+
+            SaveFD.Title = "Save Dialog";
+            SaveFD.Filter = "Bitmap Images (*.bmp) | *.bmp|All files (*.*)|*.*";
+            SaveFD.ShowDialog();
+
+            try
+            {
+                Canvas.Save(SaveFD.FileName);
+                MessageBox.Show(caption: "Saved", text: "Saved Succesfully", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Information);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(text: error.Message);
+            }
+        }
+
+        private void SINCOSTANCTG_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+             (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void AddGraph()
+        {
+            float x1 = (float)TAPBxCanvas.Width / 40;
+            float y1 = (float)TAPBxCanvas.Height / 30;
+
+            g.DrawLine(BlackPen, 0, TAPBxCanvas.Height / 2, TAPBxCanvas.Width, TAPBxCanvas.Height / 2);
+            g.DrawLine(BlackPen, TAPBxCanvas.Width / 2, 0, TAPBxCanvas.Width / 2, TAPBxCanvas.Height);
+
+
+            for (int ix = 0; ix <= Xn; ix++)
+            {
+                if (ix % 5 == 0)
+                {
+                    g.DrawLine(BlackPen, ix * x1, (TAPBxCanvas.Height / 2) + 10, ix * x1, (TAPBxCanvas.Height / 2) - 10);
+                }
+                else
+                {
+                    g.DrawLine(BlackPen, ix * x1, (TAPBxCanvas.Height / 2) + 5, ix * x1, (TAPBxCanvas.Height / 2) - 5);
+                }
+            }
+
+            for (int iy = 0; iy <= Yn; iy++)
+            {
+                if (iy % 5 == 0)
+                {
+                    g.DrawLine(BlackPen, (TAPBxCanvas.Width / 2) + 10, iy * y1, (TAPBxCanvas.Width / 2) - 10, iy * y1);
+                }
+                else
+                {
+                    g.DrawLine(BlackPen, (TAPBxCanvas.Width / 2) + 5, iy * y1, (TAPBxCanvas.Width / 2) - 5, iy * y1);
+                }
+            }
+
+
+            GrayPen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+
+            for (double x = 0; x < TAPBxCanvas.Width; x += dx)
+            {
+                g.DrawLine(GrayPen, (int)(x * xpoint), 0, (int)(x * xpoint), TAPBxCanvas.Height);
+            }
+
+            for (double y = 0; y < TAPBxCanvas.Height; y += dy)
+            {
+                g.DrawLine(GrayPen, 0, (int)(y * ypoint), TAPBxCanvas.Width, (int)(y * ypoint));
+            }
+            TAPBxCanvas.Image = Canvas;
+        }
+
+        private double F1(double x)
+        {
+            double y = 0;
+
+            int a1 = Convert.ToInt32(SINCOSTANCTG.Text);
+            if (firstPlus.Text == "-") a1 = -a1;
+            y += a1 * Math.Sin(x);
+
+            int a2 = Convert.ToInt32(TATBxCOS.Text);
+            if (secondPlus.Text == "-") a2 = -a2;
+            y += a2 * Math.Cos(x);
+
+            return y;
+
+        }
+
+        private void DrawPoints()
+        {
+            float nxx = (float)xlopp - (float)xalg;
+            float nyy = (float)ylopp - (float)yalg;
+
+            float dx = (float)TAPBxCanvas.Width / nxx;
+            float dy = (float)TAPBxCanvas.Height / nyy;
+
+            int nx0 = TAPBxCanvas.Width / 2;
+            int ny0 = TAPBxCanvas.Height / 2;
+
+            float x1 = (float)xalg;
+            float y1 = (float)F1(x1);
+
+            for (float x2 = (float)xalg; x2 <= xlopp; x2 += 0.01F)
+            {
+                float y2 = (float)F1(x2);
+                float y2y1 = Math.Abs(y1 * dy) + Math.Abs(y2 * dy);
+                if (y2y1 < TAPBxCanvas.Height)
+                {
+                    g.DrawLine(CustomPen, (x1 * dx + nx0), (ny0 - y1 * dy), (x2 * dx + nx0), (ny0 - y2 * dy));
+                    x1 = x2;
+                    y1 = y2;
+
+                }
+                else
+                {
+                    g.DrawLine(CustomPen, (x1 * dx + nx0), (ny0 - y1 * dy), (x2 * dx + nx0), (ny0 - y2 * dy));
+                    x1 = x2;
+                    y1 = y2;
+                }
+                TAPBxCanvas.Image = Canvas;
+            }
+        }
+
+        private void TALPlusMinus_Click(object sender, EventArgs e)
+        {
+            Label symbol = (Label)sender;
+            if (symbol.Text == "+")
+            {
+                symbol.Text = "-";
+            }
+            else
+            {
+                symbol.Text = "+";
+            }
+        }
+
+        private void TABTeljed_Click(object sender, EventArgs e)
+        {
+            AddGraph();
+
+        }
+
+        private void TABJoonista_Click(object sender, EventArgs e)
+        {
+
+
+            DrawPoints();
+
+
+        }
+
+        private void TABColor_Click(object sender, EventArgs e)
+        {
+
+            if (ColorD.ShowDialog() == DialogResult.OK)
+            {
+                ColorL.BackColor = ColorD.Color;
+                CustomPen.Color = ColorD.Color;
+            }
+            else
+            {
+                ColorL.BackColor = Color.WhiteSmoke;
+            }
+
+        }
+
+        private void TABClear_Click(object sender, EventArgs e)
+        {
+            Canvas = null;
+            TAPBxCanvas.Image = Canvas;
+            Canvas = new Bitmap(TAPBxCanvas.Width, TAPBxCanvas.Height);
+            g = Graphics.FromImage(Canvas);
+
         }
     }
 }
